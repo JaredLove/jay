da38783e4b6973a4256ffebfd04c1e2e5dcd55ec
JaredLove <lovejared91@Yahoo.com>
2023-09-08 17:48:23 -0500

    pg3

--- src/App.js
+++ src/App.js
@@ -2,31 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Home from './pages/Home.js';
 import { Route, Routes, Navigate } from "react-router-dom";
 import Header from './components/Header';
-// import Loading from './components/Loading';
 import Footer from './components/Footer';
-import Contact from './components/Contact';
   const App = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-      // Simulate an asynchronous operation, such as fetching data
-      
-      setTimeout(() => {
-        setLoading(false);
-      }, 100);
-    }, []);
   return (
    
      
     <div className='hero'>
-
-      {/* {
-         loading ?
-
-         <Loading />
-
-         :
-       */}
      <header>
     <><Header/></>
      
@@ -36,7 +17,6 @@ import Contact from './components/Contact';
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/" replace />} /> 
       </Routes></header>
-    {/* } */}
     <Footer/>
     </div>
     
